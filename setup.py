354a8070e9b672fc3162446173fe265da023b6c2
jklenzing <jklenzing@gmail.com>
2019-06-17 23:45:26 -0400

    Add CDF 3.7.1

--- setup.py
+++ setup.py
@@ -44,14 +44,14 @@ env_name = None
 
 # code to find if a CDF library is already installed in default locations
 def find_CDF_base(lib_name):
-    
+
     # try environment variables
     cdf_base = os.getenv('CDF_BASE')
     if cdf_base is not None:
         # check
         if os.path.exists(cdf_base):
             return cdf_base
-            
+
     # that didn't work
     # look in default locations
     platform = sys.platform
@@ -64,7 +64,7 @@ def find_CDF_base(lib_name):
         defaults = ['c:\\CDF Distribution\\']
     else:
         raise ValueError('Unknown platform, set CDF library in setup.py.')
-        
+
     # collect a list of posible directories to search in
     # grab all dirs, select those that start with cdf
     search_dir = []
@@ -103,7 +103,7 @@ elif (platform == 'win32'):
     shared_lib_name = 'dllcdf.lib'
     extra_link_args = ['/nodefaultlib:libcd']
 else:
-    if (lib_name is None) or ((base_cdf is None) and ((os_name is None) 
+    if (lib_name is None) or ((base_cdf is None) and ((os_name is None)
                                 or (env_name is None) or (extra_link_args is None)) ):
         raise ValueError('Unknown platform, please set setup.py parameters manually.')
 
@@ -126,23 +126,23 @@ else:
     if not os.path.isdir(base_cdf):
         raise ValueError('CDF directory supplied is not an actual directory.')
     if (lib_name is None):
-        raise ValueError('Attempting to use pre-installed CDF library as directed. Please set setup.py parameters manually.')    
-        
+        raise ValueError('Attempting to use pre-installed CDF library as directed. Please set setup.py parameters manually.')
+
 
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
-CDF_PATH = os.path.join(BASEPATH, 'cdf36_3-dist')
+CDF_PATH = os.path.join(BASEPATH, 'cdf37_1-dist')
 
 class CDFBuild(build):
     def run(self):
         CDF_build(self, self.build_temp)
         build.run(self)
         return
-        
+
 def CDF_build(self, ppath):
-    
+
     # build CDF Library
     build_path = os.path.abspath(ppath)
-    
+
     # check if library already exists
     if (not os.path.isfile(os.path.join(self.build_lib, 'pysatCDF', 'lib', lib_name))) or self.force:
         cmd0 = ['make', 'clean']
@@ -156,7 +156,7 @@ def CDF_build(self, ppath):
         cmd2 = ['make',
             'INSTALLDIR='+build_path,
             'install',]
-    
+
         # clean any previous attempts
         def compile0():
             call(cmd0, cwd=CDF_PATH)
@@ -166,26 +166,26 @@ def CDF_build(self, ppath):
         # do the installation
         def compile2():
             call(cmd2, cwd=CDF_PATH)
-    
+
         self.execute(compile0, [], 'Cleaning CDF')
         self.execute(compile1, [], 'Configuring CDF')
         self.execute(compile2, [], 'Compiling CDF')
         self.execute(compile0, [], 'Cleaning CDF')
-    
+
         # copy resulting tool to library build folder
         self.mkpath(os.path.join(ppath, 'pysatCDF'))
-    
+
         if not self.dry_run:
-            self.copy_tree(os.path.join(ppath, 'include'), 
+            self.copy_tree(os.path.join(ppath, 'include'),
                             os.path.join(self.build_lib, 'pysatCDF', 'include'))
             self.mkpath(os.path.join(self.build_lib, 'pysatCDF', 'lib'))
-            self.copy_file(os.path.join(ppath, 'lib', lib_name), 
+            self.copy_file(os.path.join(ppath, 'lib', lib_name),
                             os.path.join(self.build_lib, 'pysatCDF', 'lib', lib_name))
-    
+
             if shared_lib_name is not None:
-                self.copy_file(os.path.join(ppath, 'lib', shared_lib_name), 
+                self.copy_file(os.path.join(ppath, 'lib', shared_lib_name),
                                 os.path.join(self.build_lib, 'pysatCDF', 'lib', shared_lib_name))
-    
+
     # run original build code
     #build.run(self)
     return
@@ -224,21 +224,21 @@ else:
     f2py_cdf_lib_path = ''
 
 # setup fortran extension
-#---------------------------------------------------------------------------  
+#---------------------------------------------------------------------------
 
 ext1 = numpy.distutils.core.Extension(
         name = 'pysatCDF.fortran_cdf',
-        sources = [os.path.join('pysatCDF', 'fortran_cdf.f')], 
+        sources = [os.path.join('pysatCDF', 'fortran_cdf.f')],
         include_dirs = [f2py_cdf_include_path],
         f2py_options = ['--include-paths', f2py_cdf_include_path],# '--Wall', 'n', '--Wno-tabs', 'n'],
         extra_objects = [f2py_cdf_lib_path],
-        extra_link_args = extra_link_args,  
+        extra_link_args = extra_link_args,
         )
 
 
 # call setup
 #--------------------------------------------------------------------------
-numpy.distutils.core.setup( 
+numpy.distutils.core.setup(
 
     name = 'pysatCDF',
     version = '0.2.7',
@@ -249,12 +249,12 @@ numpy.distutils.core.setup(
     long_description = 'pysatCDF is a reader for CDF files and provides '+
                        'additional support for exporting to pysat data formats (not required). '+
                        'The NASA CDF library is included.',
-    url='http://github.com/rstoneback/pysatCDF',    
+    url='http://github.com/rstoneback/pysatCDF',
     # Author details
     author='Russell Stoneback',
     author_email='rstoneba@utdallas.edu',
     #data_files = [('', ['cdf36_1-dist/CDF_copyright.txt'])],
-    
+
     # Choose your license
     license='BSD',
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -270,7 +270,7 @@ numpy.distutils.core.setup(
         'Topic :: Scientific/Engineering :: Astronomy',
         'Topic :: Scientific/Engineering :: Physics',
         'Topic :: Scientific/Engineering :: Atmospheric Science',
-        
+
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: BSD License',
 
@@ -282,9 +282,4 @@ numpy.distutils.core.setup(
         'Programming Language :: Python :: 3.6',
     ],
     install_requires = ['numpy'],
-)  
-
-
-
-
-
+)
